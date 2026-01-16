3a1aa71930e9901ec30a9ddfb52038b1ec2a4c0f
Bucki, Sebastian <sbucki@psi.pl>
2026-01-16 11:51:45 +0100

    init commit

---
+++ src/app/game-content/game-content.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-game-content',
+  imports: [],
+  templateUrl: './game-content.component.html',
+  styleUrl: './game-content.component.css'
+})
+export class GameContentComponent {
+
+}
