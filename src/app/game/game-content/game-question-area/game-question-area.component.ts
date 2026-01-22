cb421650d40b6640c5eec8edfecdd53bee6b28c1
Bucki, Sebastian <sbucki@psi.pl>
2026-01-22 13:52:03 +0100

    Add game-component

--- src/app/game/game-content/game-question-area/game-question-area.component.ts
+++ src/app/game/game-content/game-question-area/game-question-area.component.ts
@@ -3,12 +3,13 @@ import {ActivatedRoute, Router} from '@angular/router';
 import {CommonModule} from '@angular/common';
 import {QuestionComponent} from './question/question.component';
 import {QuestionAreaHeaderComponent} from './question-area-header/question-area-header.component';
+import {MATERIAL_IMPORTS} from '../../../shared/material';
 
 @Component({
   selector: 'app-game-question-area',
   templateUrl: './game-question-area.component.html',
   standalone:true,
-  imports: [CommonModule, QuestionComponent, QuestionAreaHeaderComponent],
+  imports: [MATERIAL_IMPORTS,CommonModule, QuestionComponent, QuestionAreaHeaderComponent],
   styleUrl: './game-question-area.component.css'
 })
 export class GameQuestionAreaComponent implements OnInit {
