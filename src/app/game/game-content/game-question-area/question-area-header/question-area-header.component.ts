9f13450139cc8a543b108d0dedd2cad4ca7b5cd1
Bucki, Sebastian <sbucki@psi.pl>
2026-01-22 10:51:17 +0100

    works

---
+++ src/app/game/game-content/game-question-area/question-area-header/question-area-header.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-question-area-header',
+  imports: [],
+  templateUrl: './question-area-header.component.html',
+  styleUrl: './question-area-header.component.css'
+})
+export class QuestionAreaHeaderComponent {
+
+}
