ef04c415f3146756adf0c7e89882ee238c6d859b
Bucki, Sebastian <sbucki@psi.pl>
2026-02-02 14:49:24 +0100

    #15 - add questions for fragments category #1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,8 +2,6 @@ import {Component} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
-import {TEST} from './shared/createQuestion.helper';
-import {quizData} from './shared/createQuestion2.helper';
 
 @Component({
   selector: 'app-root',
@@ -19,5 +17,5 @@ import {quizData} from './shared/createQuestion2.helper';
 export class AppComponent {
   title = 'sebaquiz3.0';
   //private priv = TEST
-  private priv2 = quizData
+  //private priv2 = quizData
 }
