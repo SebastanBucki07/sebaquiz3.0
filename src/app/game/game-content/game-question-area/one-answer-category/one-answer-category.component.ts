be9c014e5903a053f2875abbfa87cfa8eaf198ca
Bucki, Sebastian <sbucki@psi.pl>
2026-02-24 20:13:03 +0100

    #45-style-questions-with-description

--- src/app/game/game-content/game-question-area/one-answer-category/one-answer-category.component.ts
+++ src/app/game/game-content/game-question-area/one-answer-category/one-answer-category.component.ts
@@ -3,13 +3,15 @@ import { QuestionService } from '../../../../shared/question-service.service';
 import { Observable } from 'rxjs';
 import { Question } from '../../../../shared/questions/question.interface';
 import { CommonModule } from '@angular/common';
+import {MATERIAL_IMPORTS} from '../../../../shared/material';
+import {MatDivider} from '@angular/material/divider';
 
 @Component({
   selector: 'app-one-answer-category',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, MATERIAL_IMPORTS, MatDivider],
   templateUrl: './one-answer-category.component.html',
-  styleUrl: './one-answer-category.component.css',
+  styleUrl: './one-answer-category.component.scss',
 })
 export class OneAnswerCategoryComponent implements OnInit {
   question$!: Observable<Question | null>;
